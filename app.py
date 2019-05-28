688cf14969294fdb400b26486a077959fbbcd20f
Ednalyn C. De Dios <ednalyn.dedios@gmail.com>
2019-05-27 23:50:44 -0500

    add self-contained html on subdirectory

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
+
+@app.route('/domestic-violence')
+def dv():
+    return render_template('dv.html')
 
 if __name__ == "__main__":
     import waitress
