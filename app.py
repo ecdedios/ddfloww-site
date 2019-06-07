cb81b3d3ddd0fa5bed29f4e08cdf8a9d7a66459a
Ednalyn C. De Dios <ednalyn.dedios@gmail.com>
2019-06-07 10:40:47 -0500

    tweak code to match script in model.py

--- app.py
+++ app.py
@@ -9,23 +9,45 @@ def index():
 
 @app.route('/domestic-violence')
 def dv():
-    return render_template('dv.html')
+    return render_template('/domestic-violence/dv.html')
 
-@app.route('/submit_dv', methods=['GET', 'POST'])
+@app.route('/domestic-violence/submit_dv', methods=['GET', 'POST'])
 def submit_dv():
+    error = None
     if request.method == 'POST':
-        feature1 = int(float(request.form['q1']))
-        feature2 = int(float(request.form['q2']))
-        feature3 = int(float(request.form['q3']))
-        feature4 = int(float(request.form['q4']))
-        feature5 = int(float(request.form['q5']))
-        feature6 = int(float(request.form['q6']))
-        feature7 = int(float(request.form['q7']))
-        feature8 = int(float(request.form['q8']))
-        feature9 = int(float(request.form['q9']))
-        prediction = predict2.predictorizer(feature1,feature2,feature3,feature4,feature5,feature6,feature7,feature8,feature9)
-        return render_template('submit_dv.html', prediction=prediction, feature1=feature1, feature2=feature2, feature3=feature3, feature4=feature4, feature5=feature5, feature6=feature6, feature7=feature7, feature8=feature8, feature9=feature9)        
-    return render_template('dv.html')
+        if request.form['q1'] == 'x':
+            error = 'Missing answer.'
+        else:
+            feature1 = int(float(request.form['q1']))
+            feature2 = int(float(request.form['q2']))
+            feature3 = int(float(request.form['q3']))
+            feature4 = int(float(request.form['q4']))
+            feature5 = int(float(request.form['q5']))
+            feature6 = int(float(request.form['q6']))
+            feature7 = int(float(request.form['q7']))
+            feature8 = int(float(request.form['q8']))
+            feature9 = int(float(request.form['q9']))
+            prediction = predict2.predictorizer(feature1,
+                                                feature2,
+                                                feature3,
+                                                feature4,
+                                                feature5,
+                                                feature6,
+                                                feature7,
+                                                feature8,
+                                                feature9)
+            return render_template('/domestic-violence/submit_dv.html',
+                                    prediction=prediction,
+                                    feature1=feature1,
+                                    feature2=feature2,
+                                    feature3=feature3,
+                                    feature4=feature4,
+                                    feature5=feature5,
+                                    feature6=feature6,
+                                    feature7=feature7,
+                                    feature8=feature8,
+                                    feature9=feature9,)        
+        return render_template('/domestic-violence/dv.html', error=error)
 
 if __name__ == "__main__":
     import waitress
