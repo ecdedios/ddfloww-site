5d40c647a246603c9da69ed5d932b6576c7b55f0
Ednalyn C. De Dios <ednalyn.dedios@gmail.com>
2019-06-10 08:51:27 -0500

    change bg color for mobiles

--- app.py
+++ app.py
@@ -11,10 +11,6 @@ def index():
 def dv():
     return render_template('/domestic-violence/dv.html')
 
-@app.route('/domestic-violence/self-report')
-def self_report():
-    return render_template('/domestic-violence/self-report.html')
-
 @app.route('/domestic-violence/submit-dv', methods=['GET', 'POST'])
 def submit_dv():
     error = None
